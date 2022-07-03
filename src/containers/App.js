eac2cae044489430e6b137be02cb3cd0d275f2b1
Carlos Bueno <carlosbueno.contato@gmail.com>
2022-07-03 17:07:08 -0300

    adding a button to test the useEffect functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,17 +8,20 @@ import './App.css';
 function App() {
   const [robots, setRobots] = useState([])
   const [searchfield, setSearchfield] = useState('')
+  const [count, setCount] = useState(0)
 
   useEffect(() =>{
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => { setRobots(users) });
+      console.log(count);
 
-  },[] )
+  },[count] ) // only run when count changes
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value)
   }
+
   
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
@@ -28,6 +31,7 @@ function App() {
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
+        <button onClick={() => setCount(count+1)}>Click me!</button>
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
